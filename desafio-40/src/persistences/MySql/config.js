76ba45d1d78c0868cfb600c8768f72bca1374891
Alejandro <alejandrodaliaga@gmail.com>
2021-09-22 23:50:40 -0300

    update env vars

--- desafio-40/src/persistences/MySql/config.js
+++ desafio-40/src/persistences/MySql/config.js
@@ -1,10 +1,10 @@
 const options = {
     client: 'mysql',
     connection: {
-        host : '127.0.0.1',
-        user : 'root',
-        password : '',
-        database : 'ecommerce-desafios',
+        host : process.env.MYSQL_HOST,
+        user : process.env.MYSQL_USER,
+        password : process.env.MYSQL_PASSWORD,
+        database : process.env.MYSQL_DATABASE,
         typeCast: function(field, next) {
             if (field.type === 'BLOB') {
                 if(field.name === 'author'){
