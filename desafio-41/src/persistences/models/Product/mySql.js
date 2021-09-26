49f523eaadddc0414e518dfd02823a00af98b413
Alejandro <alejandrodaliaga@gmail.com>
2021-09-26 12:00:17 -0300

    Agrega DAO a entidades MySql

--- desafio-41/src/persistences/models/Product/mySql.js
+++ desafio-41/src/persistences/models/Product/mySql.js
@@ -1,17 +1,50 @@
-const MySqlCrud = require('../../MySql/MySqlCrud')
+const { knex } = require('../../MySql/knex')
+const IProductDAO = require('./IProductDAO')
+const ProductDTO = require('../../dto/ProductDTO')
 
-class ProductMySql extends MySqlCrud{
+class ProductMySqlDAO extends IProductDAO{
     static instance
-    constructor(db = 'productos'){
-        super(db)
+    constructor(db, DTO){
+        super()
+        this.db = db
+        this.DTO = DTO
     }
 
     static getInstance(){
         if(!this.instance) {
-            this.instance = new ProductMySql()
+            this.instance = new ProductMySqlDAO('productos', ProductDTO)
         }
         return this.instance
     }
+
+    async getById(id){
+        const entity = await knex.from(this.db).select('*').where('id',Number(id))
+        return new this.DTO( entity[0].id, entity[0].title, entity[0].price, entity[0].thumbnail)
+    }
+
+    async getAll(){
+        const data = await knex.from(this.db).select('*')
+        return data.map(entity => new this.DTO(entity.id, entity.title, entity.price, entity.thumbnail))
+    }
+
+    async save(data){
+        const idEntity = await knex(this.db).insert(data)
+        return await this.getById(idEntity)
+    }
+
+    async update(id, data){    
+        const updated = await knex(this.db).where('id', id).update( data )
+        if( !updated ) return undefined
+        return await this.getById(id)
+    }
+    async delete(id){
+        const entity = await this.getById(id)
+        const deleted = await knex(this.db).where('id',id).del()
+
+        if(!deleted) return undefined
+
+        return entity
+    }
 }
 
-module.exports = ProductMySql.getInstance()
+module.exports = ProductMySqlDAO.getInstance()
--- desafio-41/src/persistences/models/User/mySql.js
+++ desafio-41/src/persistences/models/User/mySql.js
@@ -1,17 +1,35 @@
-const MySqlCrud = require('../../MySql/MySqlCrud')
+const { knex } = require('../../MySql/knex')
+const IUserDAO = require('./IUserDAO')
+const UserDTO = require('../../dto/UserDTO')
 
-class UserMySql extends MySqlCrud{
+class UserMySqlDAO extends IUserDAO{
     static instance
-    constructor(db = 'usuarios'){
-        super(db)
+    constructor(db,DTO ){
+        super()
+        this.db = db
+        this.DTO = DTO
     }
 
     static getInstance(){
         if(!this.instance) {
-            this.instance = new UserMySql()
+            this.instance = new UserMySqlDAO('usuarios',UserDTO)
         }
         return this.instance
     }
+
+
+
+    async getOneBy(objectParams){   
+        const entity = await knex.from(this.db).select('*').where(objectParams)
+        return this.DTO(entity[0].id, entity[0].username, entity[0].password)
+    }
+
+    async save(data){
+        const idEntity = await knex(this.db).insert(data)
+        return await this.getOneBy({ id: idEntity })
+    }
+
+
 }
 
-module.exports = UserMySql.getInstance()
+module.exports = UserMySqlDAO.getInstance()
