3bc7c88ed6aa53b9fb4f250834431dc439cc0e27
Alejandro <alejandrodaliaga@gmail.com>
2021-09-26 11:58:31 -0300

    cambia config para evitar errores en migracion

--- desafio-41/src/persistences/MySql/config.js
+++ desafio-41/src/persistences/MySql/config.js
@@ -1,10 +1,11 @@
+
 const options = {
     client: 'mysql',
     connection: {
-        host : process.env.MYSQL_HOST,
-        user : process.env.MYSQL_USER,
-        password : process.env.MYSQL_PASSWORD,
-        database : process.env.MYSQL_DATABASE,
+        host : "127.0.0.1",
+        user : 'root',
+        password : '',
+        database : 'ecommerce-desafios',
         typeCast: function(field, next) {
             if (field.type === 'BLOB') {
                 if(field.name === 'author'){
