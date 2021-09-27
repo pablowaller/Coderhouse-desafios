bc303134b9f2647fbc17bd9e3e3ce5490af17a11
Alejandro <alejandrodaliaga@gmail.com>
2021-09-27 20:41:24 -0300

    Cambia normalize a service message

--- desafio-41/src/services/Message.js
+++ desafio-41/src/services/Message.js
@@ -1,22 +1,22 @@
 const messageRepository = require('../repository/MessageRepository')
 const { sendAdminMessage } = require('../sms/twilioSms')
+const { normalizeMessages } = require('../utils/normalizes')
 
 class MessageService {
-   constructor(messageRepository){
-       this.messageRepo = messageRepository
-   }
-
-   async getAll(){
-       return await this.messageRepo.getAll()
-   }
-
-   async sendMessage(msg){
-       
-    if(msg.text.toUpperCase().includes('ADMINISTRADOR')){
-        sendAdminMessage(msg)
+    constructor(messageRepository){
+        this.messageRepo = messageRepository
     }
-    return await this.messageRepo.save(msg)
-   }
+
+    async getAll(){
+        return normalizeMessages( await this.messageRepo.getAll() )
+    }
+
+    async sendMessage(msg){
+        if(msg.text.toUpperCase().includes('ADMINISTRADOR')){
+            sendAdminMessage(msg)
+        }
+        return await this.messageRepo.save(msg)
+    }
 }
 
 
