813536ce5cd4e74066c6e5f40cbb006f5a1f93f9
Alejandro <alejandrodaliaga@gmail.com>
2021-09-27 20:32:46 -0300

    Se agrega manejo de error con logger

--- desafio-41/src/sms/twilioSms.js
+++ desafio-41/src/sms/twilioSms.js
@@ -1,12 +1,19 @@
+const { loggerError } = require('../logger/config')
+
 const client = require('twilio')(process.env.TWILIO_ACCOUNTSID,process.env.TWILIO_AUTH_TOKEN)
 
 
 function sendAdminMessage(message){
-    return client.messages.create({
-        body: `Te ha enviado un mensaje:${message.author.nombre} con el siguiente contenido: ${message.text}`,
-        from: process.env.TWILIO_NUMBER,
-        to: process.env.TWILIO_ADMIN_NUMBER
-    })
+    try{
+        return client.messages.create({
+            body: `Te ha enviado un mensaje:${message.author.nombre} con el siguiente contenido: ${message.text}`,
+            from: process.env.TWILIO_NUMBER,
+            to: process.env.TWILIO_ADMIN_NUMBER
+        })
+    }catch(err){
+        loggerError.error(err)
+    }
+   
 }
 
 module.exports = { sendAdminMessage }
