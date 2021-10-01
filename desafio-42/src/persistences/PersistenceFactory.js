d2cd735a329689714cf9e2411176f5648580c421
Alejandro <alejandrodaliaga@gmail.com>
2021-09-30 21:38:03 -0300

    elimina console.log

--- desafio-42/src/persistences/PersistenceFactory.js
+++ desafio-42/src/persistences/PersistenceFactory.js
@@ -5,7 +5,6 @@ const config = require("../config/config")
 class PersistenceFactory {
     static getPersistence(type,modelName){
         try {
-            console.log(`Se ha elegido: ${type}`)
             return require(`./daos/${modelName}/${type}`)
         }catch(err){
             console.log('No se encontro el tipo de persistencia', type, modelName)
