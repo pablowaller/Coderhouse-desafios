e752e6331253adb2b94ef6cb1c20d17829fbc1d7
Alejandro <alejandrodaliaga@gmail.com>
2021-10-07 19:17:46 -0300

    Se actualiza graphql Schema y funciones

--- 44-Reformar-para-usar-graphql/src/graphql/index.js
+++ 44-Reformar-para-usar-graphql/src/graphql/index.js
@@ -9,39 +9,66 @@ const schema = buildSchema(`
     },
     type Mutation {
         guardarProducto(title: String!, price: Int!, thumbnail: String!) : Producto,
+        actualizarProducto(id: String!, title: String, price: Int, thumbnail: String ) : Producto
     },
     type Producto {
-        _id: String
+        id: String
         title: String
         price: Int
         thumbnail: String
-    }
-
+    },
 `)
 
 
-const getProductsById = async (args)=>{
-    const  id = args.id;
-    return await ProductService.getById(id)
+const getProductById = async (args)=>{
+    try{
+        const  id = args.id;
+        return await ProductService.getById(id)
+    }catch(err){
+        return ({ message: 'Ha ocurrido un Error' })
+    }
+   
 }
 
 const getProducts = async ()=>{
-    return await ProductService.getAll()
+    try{
+        return await ProductService.getAll()
+    }catch(err){
+        return ({ message: 'Ha ocurrido un error'})
+    }
 }
 
 var guardarProducto = async ({ title, price, thumbnail }) => {
-    return await ProductService.save({ title, price, thumbnail })
+    try{
+        return await ProductService.save({ title, price, thumbnail })
+    }catch(err){
+        return ({ message: 'Ha ocurrido un error'})
+    }
+}
+
+var actualizarProducto = async ({ id, ...data }) => {
+    try{
+        let res = await ProductService.update(id, data)
+        if(res == undefined)
+        return res
+    }catch(err){
+        return ({ message: 'Ha ocurrido un error'})
+    }
 }
 
 const root = {
     productos: getProducts,
-    producto: getProductsById,
-    guardarProducto: guardarProducto
+    producto: getProductById,
+    guardarProducto: guardarProducto,
+    actualizarProducto: actualizarProducto
 }
 
 
 module.exports = graphqlHTTP({
                                 schema: schema,
                                 rootValue: root,
-                                graphiql: true
+                                graphiql: true,
+                                customFormatErrorFn: (error) => ({
+                                    message: error.message
+                                  })
                             })
